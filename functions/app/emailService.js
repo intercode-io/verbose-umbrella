ea025e77e839c0aaff116dd259d35e7ae230cc24
Yurii Postevka <yurapostevka@gmail.com>
2020-03-16 13:55:14 +0200

    move send grid to address to env var.

--- functions/app/emailService.js
+++ functions/app/emailService.js
@@ -2,10 +2,12 @@ import { sendGridKey } from "./constants"
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(sendGridKey);
 
+const to = process.env.SENDGRID_TO_ADDRESS;
+
 export async function send(data) {
     try {
         const msg = {
-            to: 'sales@intercode.io',
+            to,
             from: data.email,
             subject: 'Sales.',
             text: data.message,
