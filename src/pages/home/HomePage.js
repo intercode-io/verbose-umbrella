68505c4e3922a493d8702a003d7c21512b4e57c3
Yurii Postevka <yurapostevka@gmail.com>
2020-03-17 12:35:29 +0200

    fix footer.

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -10,33 +10,46 @@ import Swiper from 'react-id-swiper';
 
 const HomePage = () => {
     const [swiper, setSwiper] = useState(null);
+    const isMobile = document.documentElement.clientWidth < 600;
     return (
         <>
             <div>
-                <Swiper
-                    getSwiper={setSwiper}
-                    autoHeight
-                    direction="vertical"
-                    slideClass={"home-slide"}
-                    mousewheel
-                    speed={700}
-                >
-                    <div>
-                        <Neurons swiper={swiper} />
-                    </div>
-                    <div>
-                        <ServicesWeProvide />
-                    </div>
-                    <div>
-                        <Cases />
-                    </div>
-                    <div>
-                        <OurBenefits />
-                    </div>
-                    <div>
-                        <QuestionSection />
-                    </div>
-                </Swiper>
+                {isMobile
+                    ? (
+                        <>
+                            <Neurons />
+                            <ServicesWeProvide />
+                            <Cases />
+                            <OurBenefits />
+                            <QuestionSection />
+                        </>
+                    )
+                    : (
+                        <Swiper
+                            getSwiper={setSwiper}
+                            autoHeight
+                            direction="vertical"
+                            slideClass={"home-slide"}
+                            mousewheel
+                            speed={700}
+                        >
+                            <div>
+                                <Neurons swiper={swiper} />
+                            </div>
+                            <div>
+                                <ServicesWeProvide />
+                            </div>
+                            <div>
+                                <Cases />
+                            </div>
+                            <div>
+                                <OurBenefits />
+                            </div>
+                            <div>
+                                <QuestionSection />
+                            </div>
+                        </Swiper>
+                    )}
             </div>
         </>
     );
